0d6ab48c2b3c80a9bd6560b65be1b9f01c950900
LIXTON <zabuza-10@hotmail.com>
2019-06-26 04:09:44 -0500

    Update DB Connection to remotemysql

--- restapi_php/config/Database.php
+++ restapi_php/config/Database.php
@@ -2,10 +2,11 @@
 class Database{
  
     // specify your own database credentials
-    private $host = "localhost";
-    private $db_name = "rest_api";
-    private $username = "root";
-    private $password = "123";
+    private $host = "remotemysql.com";
+    private $port = "3306";
+    private $db_name = "Q8iLAx5esI";//"rest_api";
+    private $username = "Q8iLAx5esI";//"root";
+    private $password = "an4aszYpmJ";
     public $conn;
     public $error;
  
@@ -19,7 +20,7 @@ class Database{
         $this->conn = null;
  
         try{
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO("mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->exec("set names utf8");
         }catch(PDOException $exception){
             $error["msg"] .= "Connection error: " . $exception->getMessage();
